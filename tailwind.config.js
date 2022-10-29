24ff13321811001590c50e0a8e80ab4614b49b97
Anukul Pandey <anukul.030601@gmail.com>
2022-10-29 13:54:18 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+
+module.exports =
+{
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'], theme: {
+    extend: {
+      fontFamily: {
+        xbody: ['VT323'],
+      },
+    },
+  },
+  variants: {},
+  plugins: [
+  ],
+}
